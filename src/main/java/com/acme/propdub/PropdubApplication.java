f9ce2fa3930da6bd2a693afb6a285d13128e3f31
Akero <rish1994@gmail.com>
2023-05-30 10:28:38 +0530

    first commit

---
+++ src/main/java/com/acme/propdub/PropdubApplication.java
@@ -0,0 +1,13 @@
+package com.acme.propdub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropdubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropdubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/propdub/PropdubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.propdub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropdubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
